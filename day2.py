06e2e18ba24392b7735162a5964cf6d4b88f69fc
wildpieces <58203353+wildpieces@users.noreply.github.com>
2021-12-08 14:26:38 +0000

    initial update

---
+++ day2.py
@@ -0,0 +1,43 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+
+def is_on_list(days, day):
+    if day in days:
+        return "True"
+    else:
+        return ""
+
+
+def get_x(days, n):
+    return days[n]
+
+
+def add_x(days, day):
+    return days.append(day)
+
+
+def remove_x(days, day):
+    idx = days.index(day)
+    return days.pop(idx)
+
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
